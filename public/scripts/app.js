affb5ba3c8fb6834503d71278ec4c84ecb49e6e6
Robert <robertxluo@gmail.com>
2018-09-10 14:38:08 -0700

    Install Babel

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,13 @@
+"use strict";
+
 console.log("App.js is running");
 
 // JSX - JavaScript XML
-// let template = <p>This is JSX from app.js!</p>
-// let appRoot = document.getElementById("app");
 var template = React.createElement(
-    "p",
-    null,
-    "This is JSX from app.js!"
+  "h1",
+  null,
+  "Indecision App"
 );
-
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,7 @@
+console.log("App.js is running");
+
+// JSX - JavaScript XML
+let template = <h1>Indecision App</h1>
+let appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
