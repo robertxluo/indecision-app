3f50bb2fbd876d8229a60cc51d92515e404c37ac
Robert <robertxluo@gmail.com>
2018-09-10 16:38:46 -0700

    Implement basic counter app

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,12 +2,95 @@
 
 console.log("App.js is running");
 
-// JSX - JavaScript XML
+var app = {
+    title: "Indecision App",
+    subtitle: "Put your life in the hands of a computer",
+    options: ['One', 'Two']
+};
+
 var template = React.createElement(
-  "h1",
-  null,
-  "Indecision App"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        "p",
+        null,
+        app.subtitle
+    ),
+    app.options.length > 0 ? React.createElement(
+        "p",
+        null,
+        "Here are your options"
+    ) : React.createElement(
+        "p",
+        null,
+        "No options"
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Item one"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Item two"
+        )
+    )
 );
+
+var count = 0;
+var addOne = function addOne() {
+    count++;
+    renderCounterApp();
+};
+
+var minusOne = function minusOne() {
+    count--;
+    renderCounterApp();
+};
+
+var reset = function reset() {
+    count = 0;
+    renderCounterApp();
+};
+
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+var renderCounterApp = function renderCounterApp() {
+    var template_two = React.createElement(
+        "div",
+        null,
+        React.createElement(
+            "h1",
+            null,
+            "Count: ",
+            count
+        ),
+        React.createElement(
+            "button",
+            { onClick: addOne, className: "button" },
+            "+1"
+        ),
+        React.createElement(
+            "button",
+            { onClick: minusOne, className: "button" },
+            "-1"
+        ),
+        React.createElement(
+            "button",
+            { onClick: reset, className: "button" },
+            "Reset"
+        )
+    );
+    ReactDOM.render(template_two, appRoot);
+};
+
+renderCounterApp();
--- src/app.js
+++ src/app.js
@@ -1,7 +1,51 @@
 console.log("App.js is running");
 
-// JSX - JavaScript XML
-let template = <h1>Indecision App</h1>
-let appRoot = document.getElementById("app");
+const app = {
+    title: "Indecision App",
+    subtitle: "Put your life in the hands of a computer",
+    options: ['One', 'Two']
+}
 
-ReactDOM.render(template, appRoot);
+const template = (
+    <div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        {app.options.length > 0 ? <p>Here are your options</p> : <p>No options</p>}
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+let count = 0;
+const addOne = () => {
+    count++;
+    renderCounterApp();
+}
+
+const minusOne = () => {
+    count--;
+    renderCounterApp();
+}
+
+const reset = () => {
+    count = 0;
+    renderCounterApp();
+}
+
+const appRoot = document.getElementById("app");
+
+const renderCounterApp = () => {
+    const template_two = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne} className="button">+1</button>
+            <button onClick={minusOne} className="button">-1</button>
+            <button onClick={reset} className="button">Reset</button>
+        </div>
+    );   
+    ReactDOM.render(template_two, appRoot);
+}
+
+renderCounterApp();
