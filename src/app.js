151f53359778bdf1a3a7d3f172229dc998f9e23b
Robert <robertxluo@gmail.com>
2018-09-10 18:12:17 -0700

    Implement rendering of dynamic number of elements for array

--- src/app.js
+++ src/app.js
@@ -35,8 +35,9 @@ const render = () => {
             <p>{app.options.length}</p>
             <button onClick={removeAllButton}>Remove All</button>
             <ol>
-                <li>Item one</li>
-                <li>Item two</li>
+                {
+                    app.options.map((option) => <li key={option}>{option}</li>)
+                }
             </ol>
             <form onSubmit={onFormSubmit}>
                 <input type="text" name="option"/>
