3a481e91e17910be8dc2e0d38cdeaca8e0ced1af
Robert <robertxluo@gmail.com>
2018-09-11 18:12:53 -0700

    Refactor AddOption Component

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import AddOption from './components/AppOption.js';
 
 class IndecisionApp extends React.Component {
     constructor(props) {
@@ -149,39 +150,5 @@ const Option = (props) => {
         </div>
     )
 };
-
-class AddOption extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleAddOption = this.handleAddOption.bind(this);
-        this.state = {
-            error: undefined
-        };
-    }
-    handleAddOption(e) {
-        e.preventDefault();
-
-        const option = e.target.elements.option.value.trim();
-        const error = this.props.handleAddOption(option);
-        
-        this.setState(() => ({ error }));
-
-        if (!error)
-        {
-            e.target.elements.option.value = '';
-        }
-    }
-    render() {
-        return (
-            <div>
-                {this.state.error && <p>{this.state.error}</p>}
-                <form onSubmit={this.handleAddOption}>
-                    <input type="text" name="option"/>
-                    <button>Add Option</button>
-                </form>
-            </div>
-        )
-    }
-}
 
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
