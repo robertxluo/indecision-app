c049e11d2af66df36f364558c46a2c096f328dfb
Robert <robertxluo@gmail.com>
2018-09-11 19:51:12 -0700

    Refactor Action and Header components

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import AddOption from './components/AppOption.js';
-import Option from './components/Option.js'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AddOption from './components/AddOption';
+import Option from './components/Option';
+import Action from './components/Action';
+import Header from './components/Header';
+
 
 class IndecisionApp extends React.Component {
     constructor(props) {
@@ -90,32 +93,6 @@ class IndecisionApp extends React.Component {
         )
     }
 }
-
-const Header = (props) => {
-    return (
-        <div>
-            <h1>{props.title}</h1>
-            {props.subtitle && <h2>{props.subtitle}</h2>}
-        </div>
-    )    
-};
-
-Header.defaultProps = {
-    title: 'Indecision'
-};
-
-const Action = (props) => {
-    return (
-        <div>
-            <button 
-                onClick={props.handlePick}
-                disabled={!props.hasOptions}
-            >
-                What should I do?
-            </button>
-        </div>
-    )    
-};
 
 const Options = (props) => {
     return (
