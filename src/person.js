e83db7a2425e7eb478f5255cdfff6cd78e7914a3
Robert <robertxluo@gmail.com>
2018-09-11 17:41:01 -0700

    Import React and babel presets from yarn

--- src/person.js
+++ src/person.js
@@ -1,4 +1,1 @@
 console.log("person.js is running")
-
-export const isAdult = (age) => age > 18;
-export const canDrink = (age) => age > 21;
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,6 @@
 console.log("utils.js is running")
+
+
+const isSenior = (age) => age >= 65;
+
+export {isSenior as default}
