1c534f566132a58f1a19ae5924139ead21eccc1a
Robert <robertxluo@gmail.com>
2018-09-13 10:41:10 -0700

    Center modal with flexbox

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -7,6 +7,7 @@ const OptionModal = (props) => (
         onRequestClose={props.handleClearSelectedOption}
         contentLabel="Selected Option"
         closeTimeoutMS={200}
+        className="modal"
     >
         <h3>Selected Option</h3>
         {props.selectedOption && <p>{props.selectedOption}</p>}
