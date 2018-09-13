1cb1144fae80582867f03eadb4954d5764e7fd63
Robert <robertxluo@gmail.com>
2018-09-13 10:38:12 -0700

    Stylize modal transition animation

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,6 +6,7 @@ const OptionModal = (props) => (
         isOpen={!!props.selectedOption}
         onRequestClose={props.handleClearSelectedOption}
         contentLabel="Selected Option"
+        closeTimeoutMS={200}
     >
         <h3>Selected Option</h3>
         {props.selectedOption && <p>{props.selectedOption}</p>}
